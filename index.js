0be2254466b675b2ea24516ea6497f51613430ef
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-03-12 21:08:50 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = [ 'Milo', 'Otis', 'Garfield' ];
+cats.push();
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+
+function appendCat(name) {
+    const copyCats = [...cats]
+    copyCats.push(name)
+    return copyCats
+}
+
+function prependCat(name) {
+    const prepCats = [...cats]
+    prepCats.unshift(name)
+    return prepCats
+}
+
+function removeLastCat(name) {
+    const removeCats = [...cats]
+    removeCats.pop(name)
+    return removeCats
+}
+
+function removeFirstCat(name) {
+    const removeFirst = [...cats]
+    removeFirst.shift(name)
+    return removeFirst
+}
